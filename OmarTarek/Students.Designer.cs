4a8d6820c8370c35e8f5eee4b9a8e009f7ff4a72
justomar21 <tarekes68@gmail.com>
2024-02-08 04:24:32 +0200

    Added Student CRUD as well as login validation

---
+++ OmarTarek/Students.Designer.cs
@@ -0,0 +1,361 @@
+﻿namespace C__Project.OmarTarek
+{
+    partial class Students
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            trackCB = new ComboBox();
+            returnButton = new Button();
+            updateBTN = new Button();
+            exitModiBTN = new Button();
+            deleteBTN = new Button();
+            SearchTXT = new TextBox();
+            nameValiLBL = new Label();
+            idTXT = new TextBox();
+            addBTN = new Button();
+            trackLBL = new Label();
+            nameLBL = new Label();
+            nameTXT = new TextBox();
+            dataGridView1 = new DataGridView();
+            emailValiLBL = new Label();
+            emailLBL = new Label();
+            emailTXT = new TextBox();
+            dobDP = new DateTimePicker();
+            dobLBL = new Label();
+            intakeCB = new ComboBox();
+            intakeLBL = new Label();
+            classCB = new ComboBox();
+            classLBL = new Label();
+            intakeNullBTN = new Button();
+            trackNullBTN = new Button();
+            classNullBTN = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // trackCB
+            // 
+            trackCB.FormattingEnabled = true;
+            trackCB.Location = new Point(477, 304);
+            trackCB.Name = "trackCB";
+            trackCB.Size = new Size(218, 28);
+            trackCB.TabIndex = 26;
+            trackCB.SelectedIndexChanged += trackCB_SelectedIndexChanged;
+            // 
+            // returnButton
+            // 
+            returnButton.Location = new Point(6, 9);
+            returnButton.Name = "returnButton";
+            returnButton.Size = new Size(466, 29);
+            returnButton.TabIndex = 33;
+            returnButton.Text = "Return";
+            returnButton.UseVisualStyleBackColor = true;
+            returnButton.Click += returnButton_Click;
+            // 
+            // updateBTN
+            // 
+            updateBTN.Location = new Point(573, 413);
+            updateBTN.Name = "updateBTN";
+            updateBTN.Size = new Size(74, 29);
+            updateBTN.TabIndex = 27;
+            updateBTN.Text = "Update";
+            updateBTN.UseVisualStyleBackColor = true;
+            updateBTN.Visible = false;
+            updateBTN.Click += updateBTN_Click;
+            // 
+            // exitModiBTN
+            // 
+            exitModiBTN.Location = new Point(653, 413);
+            exitModiBTN.Name = "exitModiBTN";
+            exitModiBTN.Size = new Size(144, 29);
+            exitModiBTN.TabIndex = 31;
+            exitModiBTN.Text = "Exit Modification";
+            exitModiBTN.UseVisualStyleBackColor = true;
+            exitModiBTN.Visible = false;
+            exitModiBTN.Click += exitModiBTN_Click;
+            // 
+            // deleteBTN
+            // 
+            deleteBTN.Location = new Point(477, 413);
+            deleteBTN.Name = "deleteBTN";
+            deleteBTN.Size = new Size(90, 29);
+            deleteBTN.TabIndex = 29;
+            deleteBTN.Text = "Delete";
+            deleteBTN.UseVisualStyleBackColor = true;
+            deleteBTN.Visible = false;
+            deleteBTN.Click += deleteBTN_Click;
+            // 
+            // SearchTXT
+            // 
+            SearchTXT.Location = new Point(6, 44);
+            SearchTXT.Name = "SearchTXT";
+            SearchTXT.PlaceholderText = "Search by name";
+            SearchTXT.Size = new Size(466, 27);
+            SearchTXT.TabIndex = 36;
+            SearchTXT.TextChanged += SearchTXT_TextChanged;
+            // 
+            // nameValiLBL
+            // 
+            nameValiLBL.AutoSize = true;
+            nameValiLBL.ForeColor = Color.Red;
+            nameValiLBL.Location = new Point(477, 54);
+            nameValiLBL.Name = "nameValiLBL";
+            nameValiLBL.Size = new Size(290, 20);
+            nameValiLBL.TabIndex = 35;
+            nameValiLBL.Text = "3- 50 Characters , letters and numbers only";
+            nameValiLBL.Visible = false;
+            // 
+            // idTXT
+            // 
+            idTXT.Location = new Point(6, 413);
+            idTXT.Name = "idTXT";
+            idTXT.Size = new Size(315, 27);
+            idTXT.TabIndex = 34;
+            idTXT.Visible = false;
+            // 
+            // addBTN
+            // 
+            addBTN.Location = new Point(479, 412);
+            addBTN.Name = "addBTN";
+            addBTN.Size = new Size(318, 29);
+            addBTN.TabIndex = 32;
+            addBTN.Text = "Add";
+            addBTN.UseVisualStyleBackColor = true;
+            addBTN.Click += addBTN_Click;
+            // 
+            // trackLBL
+            // 
+            trackLBL.AutoSize = true;
+            trackLBL.Location = new Point(476, 281);
+            trackLBL.Name = "trackLBL";
+            trackLBL.Size = new Size(43, 20);
+            trackLBL.TabIndex = 30;
+            trackLBL.Text = "Track";
+            // 
+            // nameLBL
+            // 
+            nameLBL.AutoSize = true;
+            nameLBL.Location = new Point(478, 1);
+            nameLBL.Name = "nameLBL";
+            nameLBL.Size = new Size(49, 20);
+            nameLBL.TabIndex = 28;
+            nameLBL.Text = "Name";
+            // 
+            // nameTXT
+            // 
+            nameTXT.Location = new Point(478, 24);
+            nameTXT.MaxLength = 50;
+            nameTXT.Name = "nameTXT";
+            nameTXT.Size = new Size(315, 27);
+            nameTXT.TabIndex = 25;
+            nameTXT.TextChanged += nameTXT_TextChanged;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(6, 77);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(466, 364);
+            dataGridView1.TabIndex = 24;
+            dataGridView1.CellDoubleClick += dataGridView1_CellDoubleClick;
+            // 
+            // emailValiLBL
+            // 
+            emailValiLBL.AutoSize = true;
+            emailValiLBL.ForeColor = Color.Red;
+            emailValiLBL.Location = new Point(478, 135);
+            emailValiLBL.Name = "emailValiLBL";
+            emailValiLBL.Size = new Size(132, 20);
+            emailValiLBL.TabIndex = 39;
+            emailValiLBL.Text = "Enter a valid email";
+            emailValiLBL.Visible = false;
+            // 
+            // emailLBL
+            // 
+            emailLBL.AutoSize = true;
+            emailLBL.Location = new Point(479, 82);
+            emailLBL.Name = "emailLBL";
+            emailLBL.Size = new Size(46, 20);
+            emailLBL.TabIndex = 38;
+            emailLBL.Text = "Email";
+            // 
+            // emailTXT
+            // 
+            emailTXT.Location = new Point(479, 105);
+            emailTXT.MaxLength = 50;
+            emailTXT.Name = "emailTXT";
+            emailTXT.Size = new Size(315, 27);
+            emailTXT.TabIndex = 37;
+            emailTXT.TextChanged += emailTXT_TextChanged;
+            // 
+            // dobDP
+            // 
+            dobDP.Location = new Point(477, 181);
+            dobDP.Name = "dobDP";
+            dobDP.Size = new Size(315, 27);
+            dobDP.TabIndex = 40;
+            // 
+            // dobLBL
+            // 
+            dobLBL.AutoSize = true;
+            dobLBL.Location = new Point(476, 158);
+            dobLBL.Name = "dobLBL";
+            dobLBL.Size = new Size(94, 20);
+            dobLBL.TabIndex = 41;
+            dobLBL.Text = "Date of Birth";
+            // 
+            // intakeCB
+            // 
+            intakeCB.FormattingEnabled = true;
+            intakeCB.Location = new Point(477, 245);
+            intakeCB.Name = "intakeCB";
+            intakeCB.Size = new Size(218, 28);
+            intakeCB.TabIndex = 42;
+            // 
+            // intakeLBL
+            // 
+            intakeLBL.AutoSize = true;
+            intakeLBL.Location = new Point(476, 222);
+            intakeLBL.Name = "intakeLBL";
+            intakeLBL.Size = new Size(49, 20);
+            intakeLBL.TabIndex = 43;
+            intakeLBL.Text = "Intake";
+            // 
+            // classCB
+            // 
+            classCB.FormattingEnabled = true;
+            classCB.Location = new Point(478, 361);
+            classCB.Name = "classCB";
+            classCB.Size = new Size(217, 28);
+            classCB.TabIndex = 44;
+            // 
+            // classLBL
+            // 
+            classLBL.AutoSize = true;
+            classLBL.Location = new Point(477, 338);
+            classLBL.Name = "classLBL";
+            classLBL.Size = new Size(42, 20);
+            classLBL.TabIndex = 45;
+            classLBL.Text = "Class";
+            // 
+            // intakeNullBTN
+            // 
+            intakeNullBTN.Location = new Point(703, 245);
+            intakeNullBTN.Name = "intakeNullBTN";
+            intakeNullBTN.Size = new Size(94, 29);
+            intakeNullBTN.TabIndex = 46;
+            intakeNullBTN.Text = "Set Null";
+            intakeNullBTN.UseVisualStyleBackColor = true;
+            intakeNullBTN.Click += intakeNullBTN_Click;
+            // 
+            // trackNullBTN
+            // 
+            trackNullBTN.Location = new Point(703, 304);
+            trackNullBTN.Name = "trackNullBTN";
+            trackNullBTN.Size = new Size(94, 29);
+            trackNullBTN.TabIndex = 47;
+            trackNullBTN.Text = "Set Null";
+            trackNullBTN.UseVisualStyleBackColor = true;
+            trackNullBTN.Click += trackNullBTN_Click;
+            // 
+            // classNullBTN
+            // 
+            classNullBTN.Location = new Point(703, 361);
+            classNullBTN.Name = "classNullBTN";
+            classNullBTN.Size = new Size(94, 29);
+            classNullBTN.TabIndex = 48;
+            classNullBTN.Text = "Set Null";
+            classNullBTN.UseVisualStyleBackColor = true;
+            classNullBTN.Click += classNullBTN_Click;
+            // 
+            // Students
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(classNullBTN);
+            Controls.Add(trackNullBTN);
+            Controls.Add(intakeNullBTN);
+            Controls.Add(classCB);
+            Controls.Add(classLBL);
+            Controls.Add(intakeCB);
+            Controls.Add(intakeLBL);
+            Controls.Add(dobLBL);
+            Controls.Add(dobDP);
+            Controls.Add(emailValiLBL);
+            Controls.Add(emailLBL);
+            Controls.Add(emailTXT);
+            Controls.Add(trackCB);
+            Controls.Add(returnButton);
+            Controls.Add(updateBTN);
+            Controls.Add(exitModiBTN);
+            Controls.Add(deleteBTN);
+            Controls.Add(SearchTXT);
+            Controls.Add(nameValiLBL);
+            Controls.Add(idTXT);
+            Controls.Add(addBTN);
+            Controls.Add(trackLBL);
+            Controls.Add(nameLBL);
+            Controls.Add(nameTXT);
+            Controls.Add(dataGridView1);
+            Name = "Students";
+            Text = "Students";
+            FormClosing += Students_FormClosing;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox trackCB;
+        private Button returnButton;
+        private Button updateBTN;
+        private Button exitModiBTN;
+        private Button deleteBTN;
+        private TextBox SearchTXT;
+        private Label nameValiLBL;
+        private TextBox idTXT;
+        private Button addBTN;
+        private Label trackLBL;
+        private Label nameLBL;
+        private TextBox nameTXT;
+        private DataGridView dataGridView1;
+        private Label emailValiLBL;
+        private Label emailLBL;
+        private TextBox emailTXT;
+        private DateTimePicker dobDP;
+        private Label dobLBL;
+        private ComboBox intakeCB;
+        private Label intakeLBL;
+        private ComboBox classCB;
+        private Label classLBL;
+        private Button intakeNullBTN;
+        private Button trackNullBTN;
+        private Button classNullBTN;
+    }
+}
---
+++ OmarTarek/Students.cs
@@ -0,0 +1,279 @@
+﻿using C__Project.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TreeView;
+
+namespace C__Project.OmarTarek
+{
+    public partial class Students : Form
+    {
+        Form previousForm;
+        ExamSystemContext Context;
+        Regex nameRegex = new Regex(@"^(?=.{3,50}$)[a-zA-Z0-9]+(?:[' -][a-zA-Z0-9]+)*$");
+        Regex emailRegex = new Regex(@"^[a-zA-Z0-9._%+-]+(?:[a-zA-Z0-9-][a-zA-Z0-9._%+-]*@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4})$");
+        public Students(Form previousForm)
+        {
+            InitializeComponent();
+            this.previousForm = previousForm;
+            Context = new ExamSystemContext();
+            GetData();
+            addBTN.Enabled = false;
+            dobDP.MinDate = DateTime.Now.AddYears(-85).Date;
+            dobDP.MaxDate = DateTime.Now.AddYears(-15).Date;
+            dobDP.Value = dobDP.MaxDate.Date;
+        }
+        public void GetData()
+        {
+            dynamic classes = null;
+            int? currentBranch = (int?)trackCB.SelectedValue == null ? null : Context.Tracks
+                .Include(b => b.Department)
+                .Where(trk => trk.Id == (int)trackCB.SelectedValue).Select(t => t.Department.BranchId).FirstOrDefault();
+            var intakes = Context.Intakes.Select(i => new { Name = i.Name, ID = i.Id }).OrderBy(i => i.Name).ToList();
+            var tracks = Context.Tracks.Select(t => new { Name = t.Name, ID = t.Id }).OrderBy(t => t.Name).ToList();
+            if (currentBranch != null)
+            {
+                classes = Context.Classes
+                .Where(c => c.BranchId == currentBranch)
+                .Select(c => new { Name = c.Name, ID = c.Id }).OrderBy(c => c.Name).ToList();
+            }
+            dataGridView1.DataSource = Context.Students
+                .Include(student => student.Intake)
+                .Include(student => student.Track)
+                .Include(student => student.Class)
+                .ThenInclude(c => c.Branch)
+                .Where(student => student.Name.Contains(SearchTXT.Text.Trim()))
+                .Select(student => new
+                {
+                    student.Id,
+                    student.IntakeId,
+                    student.TrackId,
+                    student.ClassId,
+                    student.Name,
+                    student.Email,
+                    student.DOB,
+                    Intake = (string?)student.Intake.Name,
+                    Track = (string?)student.Track.Name,
+                    Class = (string?)student.Class.Name,
+                    Branch = (string?)student.Class.Branch.Name
+                })
+                .OrderBy(student => student.Class)
+                .ThenBy(student => student.Track)
+                .ThenBy(student => student.Branch)
+                .ThenBy(student => student.Intake)
+                .ToList();
+
+            intakeCB.DataSource = intakes;
+            intakeCB.DisplayMember = "Name";
+            intakeCB.SelectedIndex = -1;
+            intakeCB.ValueMember = "ID";
+            intakeCB.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            trackCB.DataSource = tracks;
+            trackCB.DisplayMember = "Name";
+            trackCB.SelectedIndex = -1;
+            trackCB.ValueMember = "ID";
+            trackCB.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            classCB.DataSource = classes;
+            classCB.DisplayMember = "Name";
+            classCB.SelectedIndex = -1;
+            classCB.ValueMember = "ID";
+            classCB.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            dataGridView1.Columns["Id"].Visible = false;
+            dataGridView1.Columns["IntakeId"].Visible = false;
+            dataGridView1.Columns["TrackId"].Visible = false;
+            dataGridView1.Columns["ClassId"].Visible = false;
+            dataGridView1.TopLeftHeaderCell.Value = "Edit";
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dataGridView1.Columns[10].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridView1.AutoResizeColumns();
+
+        }
+        private void GetTrackClasses()
+        {
+            dynamic classes = null;
+            int? currentBranch = (int?)trackCB.SelectedValue == null ? null : Context.Tracks
+                .Include(b => b.Department)
+                .Where(trk => trk.Id == (int)trackCB.SelectedValue).Select(t => t.Department.BranchId).FirstOrDefault();
+            var tracks = Context.Tracks.Select(t => new { Name = t.Name, ID = t.Id }).OrderBy(t => t.Name).ToList();
+            if (currentBranch != null)
+            {
+                classes = Context.Classes
+                .Where(c => c.BranchId == currentBranch)
+                .Select(c => new { Name = c.Name, ID = c.Id }).OrderBy(c => c.Name).ToList();
+            }
+            classCB.DataSource = classes;
+        }
+
+        private void Students_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            previousForm.Show();
+        }
+        public void RegexTest()
+        {
+            bool nameVali = nameRegex.IsMatch(nameTXT.Text.Trim());
+            bool emailVali = emailRegex.IsMatch(emailTXT.Text.Trim());
+
+            if (nameVali && emailVali)
+            { addBTN.Enabled = true; updateBTN.Enabled = true; }
+            else
+            { addBTN.Enabled = false; updateBTN.Enabled = false; }
+
+        }
+
+        private void nameTXT_TextChanged(object sender, EventArgs e)
+        {
+            bool nameVali = nameRegex.IsMatch(nameTXT.Text.Trim());
+            if (nameVali) { nameValiLBL.Visible = false; } else { nameValiLBL.Visible = true; }
+            RegexTest();
+        }
+
+        private void emailTXT_TextChanged(object sender, EventArgs e)
+        {
+            bool emailVali = emailRegex.IsMatch(emailTXT.Text.Trim());
+            if (emailVali) { emailValiLBL.Visible = false; } else { emailValiLBL.Visible = true; }
+            RegexTest();
+        }
+
+        private void addBTN_Click(object sender, EventArgs e)
+        {
+            Student s = new Student()
+            {
+                Name = nameTXT.Text.Trim(),
+                Email = emailTXT.Text.Trim(),
+                DOB = dobDP.Value.Date,
+                IntakeId = (int?)intakeCB.SelectedValue,
+                TrackId = (int?)trackCB.SelectedValue,
+                ClassId = (int?)classCB.SelectedValue
+            };
+            Context.Students.Add(s);
+            addStudentEmail(s);
+            Context.SaveChanges();
+            EndModification();
+            GetData();
+        }
+        private void addStudentEmail(Student s , string password = "123456789sS" )
+        {
+            Login log = new Login() { Email = s.Email , Password = password , Type ="Student"};
+            Context.Logins.Add(log);
+        }
+        private void EndModification()
+        {
+            emailTXT.Text = nameTXT.Text = idTXT.Text = "";
+            dobDP.Value = dobDP.MaxDate;
+            nameValiLBL.Visible = false;
+            emailValiLBL.Visible = false;
+            intakeCB.SelectedIndex = -1;
+            trackCB.SelectedIndex = -1;
+            classCB.SelectedIndex = -1;
+            addBTN.Visible = true;
+            updateBTN.Visible = false;
+            deleteBTN.Visible = false;
+            exitModiBTN.Visible = false;
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == -1 && e.RowIndex != -1)
+            {
+                idTXT.Text = dataGridView1[0, e.RowIndex].Value.ToString();
+                intakeCB.SelectedValue = dataGridView1[1, e.RowIndex].Value ?? -1;
+                trackCB.SelectedValue = dataGridView1[2, e.RowIndex].Value ?? -1;
+                classCB.SelectedValue = dataGridView1[3, e.RowIndex].Value ?? -1;
+                nameTXT.Text = dataGridView1[4, e.RowIndex].Value.ToString();
+                emailTXT.Text = dataGridView1[5, e.RowIndex].Value.ToString();
+                dobDP.Value = (DateTime)dataGridView1[6, e.RowIndex].Value;
+
+                addBTN.Visible = false;
+                updateBTN.Visible = true;
+                deleteBTN.Visible = true;
+                exitModiBTN.Visible = true;
+            }
+        }
+
+        private void updateBTN_Click(object sender, EventArgs e)
+        {
+            Student student = Context.Students.Where(student => student.Id == int.Parse(idTXT.Text)).SingleOrDefault();
+            string oldEmail = student.Email;
+            student.Name = nameTXT.Text.Trim();
+            student.Email = emailTXT.Text.Trim();
+            student.DOB = dobDP.Value.Date;
+            student.IntakeId = (int?)intakeCB.SelectedValue;
+            student.TrackId = (int?)trackCB.SelectedValue;
+            student.ClassId = (int?)classCB.SelectedValue;
+            updateStudentEmail(oldEmail , student);
+            Context.SaveChanges();
+            EndModification();
+            GetData();
+        }
+
+        private void updateStudentEmail(string oldEmail , Student s)
+        {
+            var log = Context.Logins.Where(log=> log.Email == oldEmail && log.Type == "Student").SingleOrDefault();
+            string password = log.Password;
+            deleteStudentEmail(oldEmail);
+            addStudentEmail(s, password);
+            
+        }
+
+        private void deleteBTN_Click(object sender, EventArgs e)
+        {
+            Student student = Context.Students.Where(student => student.Id == int.Parse(idTXT.Text)).SingleOrDefault();
+            deleteStudentEmail(student.Email);
+            Context.Students.Remove(student);
+            Context.SaveChanges();
+            EndModification();
+            GetData();
+        }
+        private void deleteStudentEmail(string email)
+        {
+            var log = Context.Logins.Where(log => log.Email == email && log.Type == "Student").SingleOrDefault();
+            Context.Logins.Remove(log);
+        }
+        private void exitModiBTN_Click(object sender, EventArgs e)
+        {
+            EndModification();
+        }
+
+        private void returnButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void SearchTXT_TextChanged(object sender, EventArgs e)
+        {
+            GetData();
+        }
+
+        private void trackCB_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            GetTrackClasses();
+        }
+
+        private void intakeNullBTN_Click(object sender, EventArgs e)
+        {
+            intakeCB.SelectedIndex = -1;
+        }
+
+        private void trackNullBTN_Click(object sender, EventArgs e)
+        {
+            trackCB.SelectedIndex = -1;
+        }
+
+        private void classNullBTN_Click(object sender, EventArgs e)
+        {
+            classCB.SelectedIndex = -1;
+        }
+    }
+}
