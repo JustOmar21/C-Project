69e1b1685bfe5eca51a2072c18742022c70c9b29
justomar21 <tarekes68@gmail.com>
2024-02-11 17:27:14 +0200

    Added Form1 Testing Hub

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,125 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 0;
+            button1.Text = "Branches";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(112, 12);
+            button2.Name = "button2";
+            button2.Size = new Size(111, 29);
+            button2.TabIndex = 1;
+            button2.Text = "Departments";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(229, 12);
+            button3.Name = "button3";
+            button3.Size = new Size(111, 29);
+            button3.TabIndex = 2;
+            button3.Text = "Tracks";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(12, 69);
+            button4.Name = "button4";
+            button4.Size = new Size(94, 29);
+            button4.TabIndex = 3;
+            button4.Text = "Intakes";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(112, 69);
+            button5.Name = "button5";
+            button5.Size = new Size(111, 29);
+            button5.TabIndex = 4;
+            button5.Text = "Classes";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(229, 69);
+            button6.Name = "button6";
+            button6.Size = new Size(111, 29);
+            button6.TabIndex = 5;
+            button6.Text = "Students";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(12, 122);
+            button7.Margin = new Padding(2, 2, 2, 2);
+            button7.Name = "button7";
+            button7.Size = new Size(328, 27);
+            button7.TabIndex = 6;
+            button7.Text = "Student Exam";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(12, 170);
+            button8.Margin = new Padding(2);
+            button8.Name = "button8";
+            button8.Size = new Size(328, 27);
+            button8.TabIndex = 7;
+            button8.Text = "Login";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
     }
 }
