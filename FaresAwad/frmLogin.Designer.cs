e4d18213a5e8d2b0bfb4b2b3c051a1c746dbbc8b
Fares Hassan <fareshaxxan@gmail.com>
2024-02-11 02:59:09 +0200

    Initial implementation of login and registration.

---
+++ FaresAwad/frmLogin.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace C__Project.FaresAwad
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label6 = new Label();
+            label5 = new Label();
+            button2 = new Button();
+            btnLogin = new Button();
+            checkbxShowPas = new CheckBox();
+            txtPassword = new TextBox();
+            label3 = new Label();
+            txtEmail = new TextBox();
+            label2 = new Label();
+            label1 = new Label();
+            label4 = new Label();
+            cmbUserType = new ComboBox();
+            SuspendLayout();
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Cursor = Cursors.Hand;
+            label6.Font = new Font("Nirmala UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.FromArgb(116, 86, 174);
+            label6.Location = new Point(96, 438);
+            label6.Name = "label6";
+            label6.Size = new Size(116, 20);
+            label6.TabIndex = 16;
+            label6.Text = "Create Account";
+            label6.Click += label6_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Nirmala UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(63, 416);
+            label5.Name = "label5";
+            label5.Size = new Size(185, 20);
+            label5.TabIndex = 17;
+            label5.Text = "Already Have an Account";
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.White;
+            button2.Cursor = Cursors.Hand;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.ForeColor = Color.FromArgb(116, 86, 174);
+            button2.Location = new Point(47, 365);
+            button2.Name = "button2";
+            button2.Size = new Size(216, 35);
+            button2.TabIndex = 14;
+            button2.Text = "Clear";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // btnLogin
+            // 
+            btnLogin.BackColor = Color.FromArgb(116, 86, 174);
+            btnLogin.Cursor = Cursors.Hand;
+            btnLogin.FlatAppearance.BorderSize = 0;
+            btnLogin.FlatStyle = FlatStyle.Flat;
+            btnLogin.ForeColor = Color.White;
+            btnLogin.Location = new Point(47, 324);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(216, 35);
+            btnLogin.TabIndex = 15;
+            btnLogin.Text = "Login";
+            btnLogin.UseVisualStyleBackColor = false;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // checkbxShowPas
+            // 
+            checkbxShowPas.AutoSize = true;
+            checkbxShowPas.Cursor = Cursors.Hand;
+            checkbxShowPas.FlatStyle = FlatStyle.Flat;
+            checkbxShowPas.Font = new Font("Nirmala UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            checkbxShowPas.Location = new Point(124, 286);
+            checkbxShowPas.Name = "checkbxShowPas";
+            checkbxShowPas.Size = new Size(136, 24);
+            checkbxShowPas.TabIndex = 13;
+            checkbxShowPas.Text = "Show Password";
+            checkbxShowPas.UseVisualStyleBackColor = true;
+            checkbxShowPas.CheckedChanged += checkbxShowPas_CheckedChanged;
+            // 
+            // txtPassword
+            // 
+            txtPassword.BackColor = Color.FromArgb(230, 231, 233);
+            txtPassword.BorderStyle = BorderStyle.None;
+            txtPassword.Font = new Font("Calibri", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtPassword.Location = new Point(47, 255);
+            txtPassword.Multiline = true;
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(216, 28);
+            txtPassword.TabIndex = 11;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Nirmala UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(47, 232);
+            label3.Name = "label3";
+            label3.Size = new Size(76, 20);
+            label3.TabIndex = 8;
+            label3.Text = "Password";
+            // 
+            // txtEmail
+            // 
+            txtEmail.BackColor = Color.FromArgb(230, 231, 233);
+            txtEmail.BorderStyle = BorderStyle.None;
+            txtEmail.Font = new Font("Calibri", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtEmail.Location = new Point(47, 118);
+            txtEmail.Multiline = true;
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(216, 28);
+            txtEmail.TabIndex = 12;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Nirmala UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(47, 95);
+            label2.Name = "label2";
+            label2.Size = new Size(47, 20);
+            label2.TabIndex = 9;
+            label2.Text = "Email";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("MS UI Gothic", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.FromArgb(116, 86, 174);
+            label1.Location = new Point(47, 33);
+            label1.Name = "label1";
+            label1.Size = new Size(194, 34);
+            label1.TabIndex = 6;
+            label1.Text = "Get Started";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Nirmala UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(47, 162);
+            label4.Name = "label4";
+            label4.Size = new Size(42, 20);
+            label4.TabIndex = 9;
+            label4.Text = "Type";
+            // 
+            // cmbUserType
+            // 
+            cmbUserType.BackColor = Color.FromArgb(230, 231, 233);
+            cmbUserType.FlatStyle = FlatStyle.Flat;
+            cmbUserType.Font = new Font("Calibri", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            cmbUserType.FormattingEnabled = true;
+            cmbUserType.Items.AddRange(new object[] { "Admin", "Manager", "Instructor", "Student" });
+            cmbUserType.Location = new Point(47, 185);
+            cmbUserType.Name = "cmbUserType";
+            cmbUserType.Size = new Size(216, 32);
+            cmbUserType.TabIndex = 18;
+            // 
+            // frmLogin
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(325, 489);
+            Controls.Add(cmbUserType);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(button2);
+            Controls.Add(btnLogin);
+            Controls.Add(checkbxShowPas);
+            Controls.Add(txtPassword);
+            Controls.Add(label3);
+            Controls.Add(txtEmail);
+            Controls.Add(label4);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Font = new Font("Nirmala UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            ForeColor = Color.FromArgb(164, 165, 169);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Name = "frmLogin";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Examination System";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label6;
+        private Label label5;
+        private Button button2;
+        private Button btnLogin;
+        private CheckBox checkbxShowPas;
+        private TextBox txtPassword;
+        private Label label3;
+        private TextBox txtEmail;
+        private Label label2;
+        private Label label1;
+        private Label label4;
+        private ComboBox cmbUserType;
+    }
+}
