6cfc781fce9842a2e91d561aa89c2b4448d69970
Fares Hassan <fareshaxxan@gmail.com>
2024-02-15 00:18:19 +0200

    Final Task - First Commit.

--- FaresAwad/InstructorForm.Designer.cs
+++ FaresAwad/InstructorForm.Designer.cs
@@ -28,12 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "InstructorForm";
+            dataGridView = new DataGridView();
+            label1 = new Label();
+            btnAdd = new Button();
+            btnDisplay = new Button();
+            btnEdit = new Button();
+            btnDelete = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            dataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView.Location = new Point(12, 32);
+            dataGridView.Name = "dataGridView";
+            dataGridView.RowHeadersWidth = 51;
+            dataGridView.Size = new Size(488, 236);
+            dataGridView.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(76, 282);
+            label1.Name = "label1";
+            label1.Size = new Size(358, 32);
+            label1.TabIndex = 1;
+            label1.Text = "Choose what you want to do:";
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(76, 389);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(358, 29);
+            btnAdd.TabIndex = 2;
+            btnAdd.Text = "Adding a Question";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += btnAdd_Click;
+            // 
+            // btnDisplay
+            // 
+            btnDisplay.Location = new Point(76, 340);
+            btnDisplay.Name = "btnDisplay";
+            btnDisplay.Size = new Size(358, 29);
+            btnDisplay.TabIndex = 3;
+            btnDisplay.Text = "Displaying the Courses the instructor teaches";
+            btnDisplay.UseVisualStyleBackColor = true;
+            btnDisplay.Click += btnDisplay_Click;
+            // 
+            // btnEdit
+            // 
+            btnEdit.Location = new Point(76, 441);
+            btnEdit.Name = "btnEdit";
+            btnEdit.Size = new Size(358, 29);
+            btnEdit.TabIndex = 4;
+            btnEdit.Text = "Editing a question";
+            btnEdit.UseVisualStyleBackColor = true;
+            btnEdit.Click += btnEdit_Click;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(76, 495);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(358, 29);
+            btnDelete.TabIndex = 5;
+            btnDelete.Text = "Deleting a question";
+            btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.Click += btnDelete_Click;
+            // 
+            // InstructorForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(521, 549);
+            Controls.Add(btnDelete);
+            Controls.Add(btnEdit);
+            Controls.Add(btnDisplay);
+            Controls.Add(btnAdd);
+            Controls.Add(label1);
+            Controls.Add(dataGridView);
+            Name = "InstructorForm";
+            Text = "InstructorForm";
+            ((System.ComponentModel.ISupportInitialize)dataGridView).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private DataGridView dataGridView;
+        private Label label1;
+        private Button btnAdd;
+        private Button btnDisplay;
+        private Button btnEdit;
+        private Button btnDelete;
     }
 }
--- FaresAwad/InstructorForm.cs
+++ FaresAwad/InstructorForm.cs
@@ -1,11 +1,6 @@
-﻿using System;
+﻿using C__Project.Models;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace C__Project.FaresAwad
@@ -15,6 +10,46 @@ namespace C__Project.FaresAwad
         public InstructorForm()
         {
             InitializeComponent();
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            List<Course> courses = GetInstructorCourses();
+
+            dataGridView.DataSource = courses;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            AddQuestionForm addCourseForm = new AddQuestionForm();
+            addCourseForm.ShowDialog();
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            EditQuestionForm editCourseForm = new EditQuestionForm();
+            editCourseForm.ShowDialog();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            DeleteQuestionForm deleteCourseForm = new DeleteQuestionForm();
+            deleteCourseForm.ShowDialog();
+        }
+
+        private List<Course> GetInstructorCourses()
+        {
+            int instructorId = UserSession.Id;
+
+            using (var dbContext = new ExamSystemContext())
+            {
+                var courses = dbContext.TeachesAts
+                    .Where(ta => ta.InstructorId == instructorId)
+                    .Select(ta => ta.Course)
+                    .ToList();
+
+                return courses;
+            }
         }
     }
 }
