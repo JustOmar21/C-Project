c5d4a408bf7b19d7ad5eb9c659cbc6817e5ea28f
justomar21 <tarekes68@gmail.com>
2024-02-15 01:52:21 +0200

    Added Manager starting page

---
+++ OmarTarek/Manager Forms/ManagerProfile.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace C__Project.OmarTarek.Manager_Forms
+{
+    partial class ManagerProfile
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            coursesBTN = new Button();
+            studentBTN = new Button();
+            classesBTN = new Button();
+            intakeBTN = new Button();
+            trackBTN = new Button();
+            deptBTN = new Button();
+            branchBTN = new Button();
+            salaryTXT = new Label();
+            salaryLBL = new Label();
+            dobTXT = new Label();
+            dobLBL = new Label();
+            emailTXT = new Label();
+            emailLBL = new Label();
+            nameTXT = new Label();
+            nameLBL = new Label();
+            IDTXT = new Label();
+            idLBL = new Label();
+            returnBTN = new Button();
+            SuspendLayout();
+            // 
+            // coursesBTN
+            // 
+            coursesBTN.Location = new Point(677, 180);
+            coursesBTN.Margin = new Padding(3, 4, 3, 4);
+            coursesBTN.Name = "coursesBTN";
+            coursesBTN.Size = new Size(111, 29);
+            coursesBTN.TabIndex = 18;
+            coursesBTN.Text = "Courses";
+            coursesBTN.UseVisualStyleBackColor = true;
+            coursesBTN.Click += coursesBTN_Click;
+            // 
+            // studentBTN
+            // 
+            studentBTN.Location = new Point(677, 226);
+            studentBTN.Name = "studentBTN";
+            studentBTN.Size = new Size(111, 29);
+            studentBTN.TabIndex = 17;
+            studentBTN.Text = "Students";
+            studentBTN.UseVisualStyleBackColor = true;
+            studentBTN.Click += studentBTN_Click;
+            // 
+            // classesBTN
+            // 
+            classesBTN.Location = new Point(677, 323);
+            classesBTN.Name = "classesBTN";
+            classesBTN.Size = new Size(111, 29);
+            classesBTN.TabIndex = 16;
+            classesBTN.Text = "Classes";
+            classesBTN.UseVisualStyleBackColor = true;
+            classesBTN.Click += classesBTN_Click;
+            // 
+            // intakeBTN
+            // 
+            intakeBTN.Location = new Point(677, 275);
+            intakeBTN.Name = "intakeBTN";
+            intakeBTN.Size = new Size(111, 29);
+            intakeBTN.TabIndex = 15;
+            intakeBTN.Text = "Intakes";
+            intakeBTN.UseVisualStyleBackColor = true;
+            intakeBTN.Click += intakeBTN_Click;
+            // 
+            // trackBTN
+            // 
+            trackBTN.Location = new Point(677, 131);
+            trackBTN.Name = "trackBTN";
+            trackBTN.Size = new Size(111, 29);
+            trackBTN.TabIndex = 14;
+            trackBTN.Text = "Tracks";
+            trackBTN.UseVisualStyleBackColor = true;
+            trackBTN.Click += trackBTN_Click;
+            // 
+            // deptBTN
+            // 
+            deptBTN.Location = new Point(677, 83);
+            deptBTN.Name = "deptBTN";
+            deptBTN.Size = new Size(111, 29);
+            deptBTN.TabIndex = 13;
+            deptBTN.Text = "Departments";
+            deptBTN.UseVisualStyleBackColor = true;
+            deptBTN.Click += deptBTN_Click;
+            // 
+            // branchBTN
+            // 
+            branchBTN.Location = new Point(677, 34);
+            branchBTN.Name = "branchBTN";
+            branchBTN.Size = new Size(111, 29);
+            branchBTN.TabIndex = 12;
+            branchBTN.Text = "Branches";
+            branchBTN.UseVisualStyleBackColor = true;
+            branchBTN.Click += branchBTN_Click;
+            // 
+            // salaryTXT
+            // 
+            salaryTXT.AutoSize = true;
+            salaryTXT.Location = new Point(74, 315);
+            salaryTXT.Name = "salaryTXT";
+            salaryTXT.Size = new Size(50, 20);
+            salaryTXT.TabIndex = 28;
+            salaryTXT.Text = "label1";
+            // 
+            // salaryLBL
+            // 
+            salaryLBL.AutoSize = true;
+            salaryLBL.Location = new Point(12, 315);
+            salaryLBL.Name = "salaryLBL";
+            salaryLBL.Size = new Size(52, 20);
+            salaryLBL.TabIndex = 27;
+            salaryLBL.Text = "Salary:";
+            // 
+            // dobTXT
+            // 
+            dobTXT.AutoSize = true;
+            dobTXT.Location = new Point(74, 258);
+            dobTXT.Name = "dobTXT";
+            dobTXT.Size = new Size(50, 20);
+            dobTXT.TabIndex = 26;
+            dobTXT.Text = "label1";
+            // 
+            // dobLBL
+            // 
+            dobLBL.AutoSize = true;
+            dobLBL.Location = new Point(12, 258);
+            dobLBL.Name = "dobLBL";
+            dobLBL.Size = new Size(51, 20);
+            dobLBL.TabIndex = 25;
+            dobLBL.Text = "DOB  :";
+            // 
+            // emailTXT
+            // 
+            emailTXT.AutoSize = true;
+            emailTXT.Location = new Point(74, 204);
+            emailTXT.Name = "emailTXT";
+            emailTXT.Size = new Size(50, 20);
+            emailTXT.TabIndex = 24;
+            emailTXT.Text = "label1";
+            // 
+            // emailLBL
+            // 
+            emailLBL.AutoSize = true;
+            emailLBL.Location = new Point(12, 204);
+            emailLBL.Name = "emailLBL";
+            emailLBL.Size = new Size(53, 20);
+            emailLBL.TabIndex = 23;
+            emailLBL.Text = "Email :";
+            // 
+            // nameTXT
+            // 
+            nameTXT.AutoSize = true;
+            nameTXT.Location = new Point(74, 146);
+            nameTXT.Name = "nameTXT";
+            nameTXT.Size = new Size(50, 20);
+            nameTXT.TabIndex = 22;
+            nameTXT.Text = "label1";
+            // 
+            // nameLBL
+            // 
+            nameLBL.AutoSize = true;
+            nameLBL.Location = new Point(12, 146);
+            nameLBL.Name = "nameLBL";
+            nameLBL.Size = new Size(56, 20);
+            nameLBL.TabIndex = 21;
+            nameLBL.Text = "Name :";
+            // 
+            // IDTXT
+            // 
+            IDTXT.AutoSize = true;
+            IDTXT.Location = new Point(74, 88);
+            IDTXT.Name = "IDTXT";
+            IDTXT.Size = new Size(50, 20);
+            IDTXT.TabIndex = 20;
+            IDTXT.Text = "label1";
+            // 
+            // idLBL
+            // 
+            idLBL.AutoSize = true;
+            idLBL.Location = new Point(12, 88);
+            idLBL.Name = "idLBL";
+            idLBL.Size = new Size(59, 20);
+            idLBL.TabIndex = 19;
+            idLBL.Text = "ID       : ";
+            // 
+            // returnBTN
+            // 
+            returnBTN.Location = new Point(677, 373);
+            returnBTN.Name = "returnBTN";
+            returnBTN.Size = new Size(111, 29);
+            returnBTN.TabIndex = 29;
+            returnBTN.Text = "Return";
+            returnBTN.UseVisualStyleBackColor = true;
+            returnBTN.Click += returnBTN_Click;
+            // 
+            // ManagerProfile
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(returnBTN);
+            Controls.Add(salaryTXT);
+            Controls.Add(salaryLBL);
+            Controls.Add(dobTXT);
+            Controls.Add(dobLBL);
+            Controls.Add(emailTXT);
+            Controls.Add(emailLBL);
+            Controls.Add(nameTXT);
+            Controls.Add(nameLBL);
+            Controls.Add(IDTXT);
+            Controls.Add(idLBL);
+            Controls.Add(coursesBTN);
+            Controls.Add(studentBTN);
+            Controls.Add(classesBTN);
+            Controls.Add(intakeBTN);
+            Controls.Add(trackBTN);
+            Controls.Add(deptBTN);
+            Controls.Add(branchBTN);
+            Name = "ManagerProfile";
+            Text = "ManagerForm";
+            FormClosing += this.ManagerProfile_FormClosing;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Button coursesBTN;
+        private Button studentBTN;
+        private Button classesBTN;
+        private Button intakeBTN;
+        private Button trackBTN;
+        private Button deptBTN;
+        private Button branchBTN;
+        private Label salaryTXT;
+        private Label salaryLBL;
+        private Label dobTXT;
+        private Label dobLBL;
+        private Label emailTXT;
+        private Label emailLBL;
+        private Label nameTXT;
+        private Label nameLBL;
+        private Label IDTXT;
+        private Label idLBL;
+        private Button returnBTN;
+    }
+}
---
+++ OmarTarek/Manager Forms/ManagerProfile.cs
@@ -0,0 +1,107 @@
+﻿using C__Project.Khateep;
+using C__Project.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C__Project.OmarTarek.Manager_Forms
+{
+    public partial class ManagerProfile : Form
+    {
+        ExamSystemContext Context = new ExamSystemContext();
+        Form previousForm;
+        public ManagerProfile(Form previousForm)
+        {
+            InitializeComponent();
+            this.previousForm = previousForm;
+        }
+
+        private void branchBTN_Click(object sender, EventArgs e)
+        {
+            Branches b = new Branches(this);
+            this.Hide();
+            b.Show();
+        }
+
+        private void deptBTN_Click(object sender, EventArgs e)
+        {
+            var branches = Context.Branches.Select(b => new { Name = b.Name, ID = b.Id }).ToList();
+            if (branches.Count == 0)
+            {
+                MessageBox.Show("You Cannot Add any Department beacause no Branches exist", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                Departments dept = new Departments(this);
+                this.Hide();
+                dept.Show();
+            }
+        }
+
+        private void trackBTN_Click(object sender, EventArgs e)
+        {
+            var department = Context.Departments.Select(b => b).ToList();
+            if (department.Count == 0)
+            {
+                MessageBox.Show("You Cannot Add any Tracks beacause no Department exists", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                Tracks track = new Tracks(this);
+                this.Hide();
+                track.Show();
+            }
+        }
+
+        private void coursesBTN_Click(object sender, EventArgs e)
+        {
+            Courses course = new Courses(this);
+            this.Hide();
+            course.Show();
+        }
+
+        private void studentBTN_Click(object sender, EventArgs e)
+        {
+            Students student = new Students(this);
+            this.Hide();
+            student.Show();
+        }
+
+        private void intakeBTN_Click(object sender, EventArgs e)
+        {
+            Intakes intake = new Intakes(this);
+            this.Hide();
+            intake.Show();
+        }
+
+        private void classesBTN_Click(object sender, EventArgs e)
+        {
+            var branches = Context.Branches.Select(b => new { Name = b.Name, ID = b.Id }).ToList();
+            if (branches.Count == 0)
+            {
+                MessageBox.Show("You Cannot Add any Department beacause no Branches exist", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                Classes Class = new Classes(this);
+                this.Hide();
+                Class.Show();
+            }
+        }
+        private void returnBTN_Click(object sender , EventArgs e)
+        {
+            this.Close();
+        }
+        private void ManagerProfile_FormClosing(object sender , FormClosingEventArgs e)
+        {
+            previousForm.Show();
+        }
+    }
+}
