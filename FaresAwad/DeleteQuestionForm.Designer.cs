3d2422a23eaff7ad1827449fbb64fbca434b6084
Fares Hassan <fareshaxxan@gmail.com>
2024-02-15 03:54:48 +0200

    Delete Question Form end.

--- FaresAwad/DeleteQuestionForm.Designer.cs
+++ FaresAwad/DeleteQuestionForm.Designer.cs
@@ -28,12 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "DeleteQuestionForm";
+            btnRemoveQuestion = new Button();
+            txtQuestionIdToRemove = new TextBox();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // btnRemoveQuestion
+            // 
+            btnRemoveQuestion.Location = new Point(82, 187);
+            btnRemoveQuestion.Name = "btnRemoveQuestion";
+            btnRemoveQuestion.Size = new Size(448, 29);
+            btnRemoveQuestion.TabIndex = 0;
+            btnRemoveQuestion.Text = "Remove";
+            btnRemoveQuestion.UseVisualStyleBackColor = true;
+            btnRemoveQuestion.Click += btnRemoveQuestion_Click;
+            // 
+            // txtQuestionIdToRemove
+            // 
+            txtQuestionIdToRemove.Location = new Point(178, 136);
+            txtQuestionIdToRemove.Name = "txtQuestionIdToRemove";
+            txtQuestionIdToRemove.Size = new Size(270, 27);
+            txtQuestionIdToRemove.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(237, 87);
+            label1.Name = "label1";
+            label1.Size = new Size(161, 20);
+            label1.TabIndex = 2;
+            label1.Text = "QuestionID To Remove";
+            // 
+            // DeleteQuestionForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(641, 313);
+            Controls.Add(label1);
+            Controls.Add(txtQuestionIdToRemove);
+            Controls.Add(btnRemoveQuestion);
+            Name = "DeleteQuestionForm";
+            Text = "DeleteQuestionForm";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnRemoveQuestion;
+        private TextBox txtQuestionIdToRemove;
+        private Label label1;
     }
 }
--- FaresAwad/DeleteQuestionForm.cs
+++ FaresAwad/DeleteQuestionForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using C__Project.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,58 @@ namespace C__Project.FaresAwad
         public DeleteQuestionForm()
         {
             InitializeComponent();
+        }
+
+        private void btnRemoveQuestion_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int questionIdToRemove;
+                if (!int.TryParse(txtQuestionIdToRemove.Text, out questionIdToRemove))
+                {
+                    MessageBox.Show("Please enter a valid question ID to remove.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                int instructorId = UserSession.Id;
+
+                if (!IsInstructorAllowedToRemoveQuestion(instructorId, questionIdToRemove))
+                {
+                    MessageBox.Show("You are not allowed to remove this question.", "Permission Denied", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                using (var dbContext = new ExamSystemContext())
+                {
+                    var questionToRemove = dbContext.Questions.Find(questionIdToRemove);
+
+                    if (questionToRemove != null)
+                    {
+                        // Remove the question from the database
+                        dbContext.Questions.Remove(questionToRemove);
+                        dbContext.SaveChanges();
+
+                        MessageBox.Show("Question removed successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Question not found in the database.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private bool IsInstructorAllowedToRemoveQuestion(int instructorId, int questionIdToRemove)
+        {
+            using (var dbContext = new ExamSystemContext())
+            {
+                // Check if the instructor is allowed to remove the question
+                return dbContext.Questions.Any(q => q.Id == questionIdToRemove && q.InstructorId == instructorId);
+            }
         }
     }
 }
