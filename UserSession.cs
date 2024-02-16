4a520729107f9a18246a498fca00dfe58548b76e
justomar21 <tarekes68@gmail.com>
2024-02-17 00:36:54 +0200

    moved static class UserSession to its own file

---
+++ UserSession.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Project
+{
+    internal static class UserSession
+    {
+        public static int? Id { get; set; }
+
+        public static void ClearUserSession()
+        {
+            Id = 0;
+        }
+    }
+}
