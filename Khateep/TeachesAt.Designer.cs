b88ae472c5d24ecf57694377e9a739f814beb3e1
khateep <ham551995@gmail.com>
2024-02-18 11:36:50 +0200

    ..

---
+++ Khateep/TeachesAt.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace C__Project.Khateep
+{
+    partial class TeachesAt
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            idTXT = new TextBox();
+            updateBTN = new Button();
+            exitModiBTN = new Button();
+            deleteBTN = new Button();
+            addBTN = new Button();
+            SearchTXT = new TextBox();
+            dataGridView1 = new DataGridView();
+            returnButton = new Button();
+            exmCB = new ComboBox();
+            course = new Label();
+            stdCB = new ComboBox();
+            student = new Label();
+            comboBox1 = new ComboBox();
+            instructor = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // idTXT
+            // 
+            idTXT.Location = new Point(444, 7);
+            idTXT.Margin = new Padding(3, 2, 3, 2);
+            idTXT.Name = "idTXT";
+            idTXT.Size = new Size(276, 23);
+            idTXT.TabIndex = 99;
+            idTXT.Visible = false;
+            // 
+            // updateBTN
+            // 
+            updateBTN.Location = new Point(443, 283);
+            updateBTN.Margin = new Padding(3, 2, 3, 2);
+            updateBTN.Name = "updateBTN";
+            updateBTN.Size = new Size(276, 22);
+            updateBTN.TabIndex = 95;
+            updateBTN.Text = "Update";
+            updateBTN.UseVisualStyleBackColor = true;
+            updateBTN.Visible = false;
+            // 
+            // exitModiBTN
+            // 
+            exitModiBTN.Location = new Point(587, 328);
+            exitModiBTN.Margin = new Padding(3, 2, 3, 2);
+            exitModiBTN.Name = "exitModiBTN";
+            exitModiBTN.Size = new Size(132, 22);
+            exitModiBTN.TabIndex = 97;
+            exitModiBTN.Text = "Exit Modification";
+            exitModiBTN.UseVisualStyleBackColor = true;
+            exitModiBTN.Visible = false;
+            // 
+            // deleteBTN
+            // 
+            deleteBTN.Location = new Point(441, 328);
+            deleteBTN.Margin = new Padding(3, 2, 3, 2);
+            deleteBTN.Name = "deleteBTN";
+            deleteBTN.Size = new Size(132, 22);
+            deleteBTN.TabIndex = 96;
+            deleteBTN.Text = "Delete";
+            deleteBTN.UseVisualStyleBackColor = true;
+            deleteBTN.Visible = false;
+            // 
+            // addBTN
+            // 
+            addBTN.Location = new Point(442, 302);
+            addBTN.Margin = new Padding(3, 2, 3, 2);
+            addBTN.Name = "addBTN";
+            addBTN.Size = new Size(276, 22);
+            addBTN.TabIndex = 98;
+            addBTN.Text = "Add";
+            addBTN.UseVisualStyleBackColor = true;
+            // 
+            // SearchTXT
+            // 
+            SearchTXT.Location = new Point(12, 63);
+            SearchTXT.Margin = new Padding(3, 2, 3, 2);
+            SearchTXT.Name = "SearchTXT";
+            SearchTXT.PlaceholderText = "Search by name";
+            SearchTXT.Size = new Size(408, 23);
+            SearchTXT.TabIndex = 94;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 88);
+            dataGridView1.Margin = new Padding(3, 2, 3, 2);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(409, 273);
+            dataGridView1.TabIndex = 93;
+            // 
+            // returnButton
+            // 
+            returnButton.Location = new Point(12, 17);
+            returnButton.Margin = new Padding(3, 2, 3, 2);
+            returnButton.Name = "returnButton";
+            returnButton.Size = new Size(409, 22);
+            returnButton.TabIndex = 92;
+            returnButton.Text = "Return";
+            returnButton.UseVisualStyleBackColor = true;
+            returnButton.Click += returnButton_Click;
+            // 
+            // exmCB
+            // 
+            exmCB.FormattingEnabled = true;
+            exmCB.Location = new Point(443, 62);
+            exmCB.Margin = new Padding(3, 2, 3, 2);
+            exmCB.Name = "exmCB";
+            exmCB.Size = new Size(276, 23);
+            exmCB.TabIndex = 90;
+            // 
+            // course
+            // 
+            course.AutoSize = true;
+            course.Location = new Point(442, 45);
+            course.Name = "course";
+            course.Size = new Size(44, 15);
+            course.TabIndex = 91;
+            course.Text = "Course";
+            // 
+            // stdCB
+            // 
+            stdCB.FormattingEnabled = true;
+            stdCB.Location = new Point(444, 114);
+            stdCB.Margin = new Padding(3, 2, 3, 2);
+            stdCB.Name = "stdCB";
+            stdCB.Size = new Size(276, 23);
+            stdCB.TabIndex = 88;
+            // 
+            // student
+            // 
+            student.AutoSize = true;
+            student.Location = new Point(443, 97);
+            student.Name = "student";
+            student.Size = new Size(48, 15);
+            student.TabIndex = 89;
+            student.Text = "Student";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(444, 166);
+            comboBox1.Margin = new Padding(3, 2, 3, 2);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(276, 23);
+            comboBox1.TabIndex = 100;
+            // 
+            // instructor
+            // 
+            instructor.AutoSize = true;
+            instructor.Location = new Point(443, 149);
+            instructor.Name = "instructor";
+            instructor.Size = new Size(58, 15);
+            instructor.TabIndex = 101;
+            instructor.Text = "Instructor";
+            // 
+            // TeachesAt
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(comboBox1);
+            Controls.Add(instructor);
+            Controls.Add(idTXT);
+            Controls.Add(updateBTN);
+            Controls.Add(exitModiBTN);
+            Controls.Add(deleteBTN);
+            Controls.Add(addBTN);
+            Controls.Add(SearchTXT);
+            Controls.Add(dataGridView1);
+            Controls.Add(returnButton);
+            Controls.Add(exmCB);
+            Controls.Add(course);
+            Controls.Add(stdCB);
+            Controls.Add(student);
+            Name = "TeachesAt";
+            Text = "TeachesAt";
+            FormClosing += TeachesAt_FormClosing;
+            Load += TeachesAt_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox idTXT;
+        private Button updateBTN;
+        private Button exitModiBTN;
+        private Button deleteBTN;
+        private Button addBTN;
+        private TextBox SearchTXT;
+        private DataGridView dataGridView1;
+        private Button returnButton;
+        private ComboBox exmCB;
+        private Label course;
+        private ComboBox stdCB;
+        private Label student;
+        private ComboBox comboBox1;
+        private Label instructor;
+    }
+}
---
+++ Khateep/TeachesAt.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using C__Project.Models;
+
+namespace C__Project.Khateep
+{
+    public partial class TeachesAt : Form
+    {
+        Form previousForm;
+        ExamSystemContext Context;
+        public TeachesAt(Form previousForm)
+        {
+            InitializeComponent();
+            this.previousForm = previousForm;
+            Context = new ExamSystemContext();
+        }
+
+        private void TeachesAt_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void returnButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+
+        }
+
+        private void TeachesAt_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            previousForm.Show();
+
+        }
+    }
+}
